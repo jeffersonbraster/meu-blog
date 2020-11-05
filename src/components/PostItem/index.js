39b482101c080922af714647f170f064c422220d
Jefferson <jeffersonbraster@gmail.com>
2020-11-04 22:43:17 -0300

    estruturando os dados no PostItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,23 +1,39 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 import * as S from "./styles"
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug/">
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47650b">News</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
         <S.PostItemDate>
-          04 de Novembro de 2020 • 4 min de leitura
+          {date} • {timeToRead} min de leitura
         </S.PostItemDate>
-        <S.PostItemTitle>Molezinha é uma lenda do geq</S.PostItemTitle>
-        <S.PostItemDescription>
-          Um verdadeiro icone do geq agora em uma nova casa a FortBrasil. Relata
-          que não existe desafios, tudo na molezinha.
-        </S.PostItemDescription>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem
