e7661ad1167f94d5d982acaf9289aa54a0c2cf15
Jefferson <jeffersonbraster@gmail.com>
2020-11-05 19:28:57 -0300

    usando o createPages do gatsby Node API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+import path from "path"
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 //adicionando o campo slug para cada um dos posts
@@ -18,4 +19,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `/${slug.slice(12)}`,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
