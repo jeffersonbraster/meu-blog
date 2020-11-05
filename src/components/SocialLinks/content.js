5619b9b1c6d3171cefcdb8718470d8739056add2
Jefferson <jeffersonbraster@gmail.com>
2020-11-04 21:18:54 -0300

    criando e estilizando o SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/jeffersonbraster`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/jefferson-brandao-dev/`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/jeffersonbrandao/`,
+  },
+]
+
+export default links
