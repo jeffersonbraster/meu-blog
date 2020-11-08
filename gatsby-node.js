ee18be3a7f6e309f15ac1fa2da40a4412b17a1c5
Jefferson <jeffersonbraster@gmail.com>
2020-11-08 00:47:40 -0300

    mudança de cover para image

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,7 +35,7 @@ exports.createPages = ({ graphql, actions }) => {
               date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
               description
               title
-              cover
+              image
             }
             timeToRead
             fields {
